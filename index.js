62df56eacba1224d178bc9aff11fb068be39fa21
Omar <o.moreta@gmail.com>
2023-10-12 23:28:27 -0400

    added 404 catch-all/wildcard page route

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// GET / (home/base url)
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// GET /* (wildcard/catch-all route)
+app.get("*", (req, res) => {
+  res.status(404).send("<h1>404 Not Found Page</h1>");
+});
 
 app.listen(PORT, function () {
-    console.log(`Server listening on port: ${PORT}`)
-})
+  console.log(`Server listening on port: ${PORT}`);
+});
